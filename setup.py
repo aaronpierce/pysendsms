1daafc031a86c12619729f1a06bc6c48b3f428a7
Aaron Pierce <aaronpierce15@gmail.com>
2020-03-13 09:51:27 -0500

    Updating package info for PyPi upload

--- setup.py
+++ setup.py
@@ -4,22 +4,23 @@ from setuptools import setup
 readme = open('README.md').read()
 
 
-setup(name=                                 'pysendsms',
-      version=                              '0.1',
-      description=                          'Send SMS via Gmail SMTP servers.',
-      long_description=                      readme,
-      long_description_content_type=        'text/markdown',
-      classifiers=[
-                                            'License :: OSI Approved :: MIT License',
-                                            'Intended Audience :: Developers',
-                                            'Programming Language :: Python'
-                                            'Programming Language :: Python :: 3',        
-                                            'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
-      keywords=                             'sms python gmail mail mms text message',
-      url=                                  'http://github.com/aaronpierce/pysendsms',
-      author=                               'Aaron Pierce',
-      author_email=                         'aaronpierce15@gmail.com',
-      license=                              'MIT',
-      include_package_data=                 True,
-      python_requires=                      '>=3')
+setup(name                              =       'pysendsms',
+      version                           =       '0.1.0',
+      description                       =       'Send SMS via Gmail SMTP servers.',
+      long_description                  =        readme,
+      long_description_content_type     =       'text/markdown',
+      classifiers                       =       [
+                                                'License :: OSI Approved :: MIT License',
+                                                'Intended Audience :: Developers',
+                                                'Programming Language :: Python'
+                                                'Programming Language :: Python :: 3',        
+                                                'Topic :: Software Development :: Libraries :: Python Modules',
+                                                ],
+      keywords                          =       'sms python gmail mail mms text message',
+      url                               =       'http://github.com/aaronpierce/pysendsms',
+      author                            =       'Aaron Pierce',
+      author_email                      =       'aaronpierce15@gmail.com',
+      license                           =       'MIT',
+      packages                          =       ['pysendsms'],
+      include_package_data              =       True,
+      python_requires                   =       '>=3')
