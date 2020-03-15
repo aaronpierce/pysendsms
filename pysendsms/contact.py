a9faad3b4366d518b2d45dced9e47222fa2c7fce
Aaron Pierce <aaron@Aarons-MacBook-Pro.local>
2020-03-14 21:01:01 -0500

    Added phone number validation

--- pysendsms/contact.py
+++ pysendsms/contact.py
@@ -1,17 +1,31 @@
 # contact.py
 
 from json import loads
+import re
 from . import data
 
 CARRIERS = loads(data.load_file())
 
+
 class Contact:
-	def __init__(self, number, carrier):
-		self.number = number.replace('-', '') if '-' in number else number
-		self.carrier = carrier
+    def __init__(self, number, carrier):
+        if not self.validate(number):
+            raise ValueError("Bad phone number")
 
-	def __repr__(self):
-		return f'{self.number}{self.carrier}'
+        self.carrier = carrier
 
-	def address(self):
-		return repr(self)
+    def __repr__(self):
+        return f'{self.number}{self.carrier}'
+
+    def address(self):
+        return repr(self)
+
+    def validate(self, number):
+        matcher = re.compile(r'^\+?\d{0,2}-?\d{3}-?\d{3}-?\d{4}$')
+        if re.match(matcher, number):
+            self.number = number.replace('-', '').replace('+', '')
+            state = True
+        else:
+            state = False
+
+        return state
