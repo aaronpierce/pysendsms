11cdce59d032f8e3c88e33a4c1218b42d3219f4f
Aaron Pierce <aaronpierce15@gmail.com>
2020-03-19 16:50:04 -0500

    Updated to minor 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 readme = open('README.md').read()
 
 setup(name                              =       'pysendsms',
-      version                           =       '0.1.8',
+      version                           =       '0.2.0',
       description                       =       'Send SMS via Gmail SMTP servers.',
       long_description                  =        readme,
       long_description_content_type     =       'text/markdown',
